d3e3a84a03842be590f7c367e4af4523574811d0
Vivian Su <vls23@duke.edu>
2022-10-13 18:42:46 -0400

    db folder created

---
+++ db/create.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Users (
+    id INT NOT NULL PRIMARY KEY,
+    email VARCHAR UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    address VARCHAR(255) NOT NULL,
+    balance INT NOT NULL
+);
