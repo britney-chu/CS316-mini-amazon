64531f964083ebaddb99bc18f5702a5ec3a9993f
Vivian Su <vls23@duke.edu>
2022-10-13 19:54:53 -0400

    more editing

--- db/create.sql
+++ db/create.sql
@@ -1,10 +1,26 @@
 CREATE TABLE Users (
     id INT NOT NULL PRIMARY KEY,
     email VARCHAR UNIQUE NOT NULL,
+    type_user ENUM("Buyer", "Seller", "Buyer+Seller") NOT NULL,
     password VARCHAR(255) NOT NULL,
     name VARCHAR(255) NOT NULL,
     address VARCHAR(255) NOT NULL,
     balance INT NOT NULL
+);
+
+CREATE TABLE Products (
+    CONSTRAINT ps_id PRIMARY KEY (pid,seller_id),
+    pid INT NOT NULL,
+    seller_id INT NOT NULL REFERENCES Users(id), 
+    category VARCHAR(255) NOT NULL,
+    short_name VARCHAR(255) NOT NULL,
+    longer_description VARCHAR(255) NOT NULL,
+    product_image VARCHAR(255) NOT NULL, 
+    price VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Cart (
+    
 );
 
 CREATE TABLE Purchases (
@@ -12,23 +28,8 @@ CREATE TABLE Purchases (
     user_id INT NOT NULL REFERENCES Users(id),
     pid INT NOT NULL REFERENCES Products(id),
     order_id INT NOT NULL REFERENCES Order(id),
-    fulfillment_status VARCHAR(255) NOT NULL 
-);
-
-CREATE TABLE Products (
-    pid INT NOT NULL PRIMARY KEY,
-    short_name VARCHAR(255),
-    category VARCHAR(255),
-    longer_description VARCHAR(255) NOT NULL,
-    product_image, 
-    price,
-    details,
-    list_of_sellers,
-
-);
-
-CREATE TABLE Cart (
-    
+    seller VARCHAR(255) NOT NULL,
+    fulfillment_status BOOLEAN NOT NULL 
 );
 
 CREATE TABLE Order (
@@ -36,9 +37,8 @@ CREATE TABLE Order (
     uid INT NOT NULL REFERENCES Users(id),
     time_order timestamp without time zone NOT NULL DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
     total_amount FLOAT NOT NULL,
-    number_items INT NOT NULL
-
-    
+    number_of_items INT NOT NULL, 
+    fulfillment_status BOOLEAN NOT NULL
 );
 
 CREATE TABLE Inventory (
